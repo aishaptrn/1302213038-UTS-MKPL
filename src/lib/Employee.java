03e320e155a9051fe09ec785fbaadfa5c2266d44
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2024-04-20 23:43:19 +0700

    Menghapus duplikasi kode pada setMonthlySalary
    Menghapus duplikasi kode pada method setMonthlySalary menggunakan Switch Case

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -7,77 +7,49 @@ import java.util.List;
 
 public class Employee {
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private LocalDate joinDate;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+	private EmployeeInfo employeeInfo;
+    private List<String> childNames;
+    private List<String> childIdNumbers;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.joinDate = joinDate;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
+	private static final int GRADE_1_SALARY = 3000000;
+    private static final int GRADE_2_SALARY = 5000000;
+    private static final int GRADE_3_SALARY = 7000000;
+
+    public Employee(EmployeeInfo employeeInfo) {
+        this.employeeInfo = employeeInfo;
+        this.childNames = new LinkedList<>();
+        this.childIdNumbers = new LinkedList<>();
+    }
 	
 	/**
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
-	}
-	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
+	public void setMonthlySalary(int grade) {
+        int salary;
+        switch (grade) {
+            case 1:
+                salary = GRADE_1_SALARY;
+                break;
+            case 2:
+                salary = GRADE_2_SALARY;
+                break;
+            case 3:
+                salary = GRADE_3_SALARY;
+                break;
+            default:
+                salary = 0;
+        }
+        if (employeeInfo.isForeigner()) {
+            salary = (int) (salary * 1.5);
+			employeeInfo.setMonthlySalary(salary);
+        }
+    }
 	
 	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+		employeeInfo.setSpouseName(spouseName);
+		employeeInfo.setSpouseIdNumber(spouseIdNumber);
 	}
 	
 	public void addChild(String childName, String childIdNumber) {
@@ -90,12 +62,13 @@ public class Employee {
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
-		if (date.getYear() == joinDate.getYear()) {
-			monthWorkingInYear = date.getMonthValue() - joinDate.getMonthValue();
+		if (date.getYear() == employeeInfo.getJoinDate().getYear()) {
+			int month = date.getMonthValue() - employeeInfo.getJoinDate().getMonthValue();
+			employeeInfo.setMonthWorkingInYear(month);
 		}else {
-			monthWorkingInYear = 12;
+			employeeInfo.setMonthWorkingInYear(12);
 		}
 		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		return TaxFunction.calculateTax(employeeInfo.getMonthlySalary(), employeeInfo.getOtherMonthlyIncome(), employeeInfo.getMonthWorkingInYear(), employeeInfo.getAnnualDeductible(), employeeInfo.getSpouseIdNumber().equals(""), childIdNumbers.size());
 	}
 }
